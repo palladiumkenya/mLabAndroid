a5c025bb76742c8c3eade3f7bf8d7b6bac0ed604
unknown <vikogutu77@gmail.com>
2022-08-04 18:55:16 +0300

    urls_layout

--- app/src/main/java/com/example/kenweezy/mytablayouts/Config/Config.java
+++ app/src/main/java/com/example/kenweezy/mytablayouts/Config/Config.java
@@ -1,11 +1,56 @@
 package com.example.kenweezy.mytablayouts.Config;
 
+import static android.R.layout.simple_spinner_item;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Config {
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.kenweezy.mytablayouts.Mylogin;
+import com.example.kenweezy.mytablayouts.R;
+import com.example.kenweezy.mytablayouts.models.urlModel;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class Config extends AppCompatActivity {
+
+    //begin selectURLs
+    urlModel url_Model;
+    ArrayList<String> urlModelArrayList;
+    ArrayList<urlModel> names;
+    Spinner spinner1;
+    int dataId;
+    SharedPreferences sharedPreferences1;
+    Button btn_prcd;
+    //End selectURLS
+
+
     public static String BASE_URL="";
     public static String mainShortcode="40147";
     public static String sendSmsShortcode="40147";
@@ -103,6 +148,156 @@ public class Config {
             "4=Single drug substitution",
             "5=Baseline VL (for infants diagnosed through EID)"
     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+
+        //selectURLS
+
+        SharedPreferences sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        btn_prcd = findViewById(R.id.login_proceed);
+        spinner1 =findViewById(R.id.spCompany);
+        getUrls();
+
+        //end SelectURLS
+
+        btn_prcd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                editor.putString("keyName", BASE_URL);
+                editor.apply();
+
+                String getValue = sharedPreferences.getString("keyName", "defaultValue");
+
+                if (getValue.equals("--select baseURL--")){
+                    Toast.makeText(Config.this, "Invalid", Toast.LENGTH_LONG).show();
+                }else if (dataId==-1) {
+                    Toast.makeText(Config.this, "Invalid", Toast.LENGTH_LONG).show();
+
+                }else{
+                    getAlert();
+                }
+
+            }
+        });
+
+    }
+    private void getAlert(){
+
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(Config.this);
+        builder1.setMessage("You have connected to"+ ""+ BASE_URL);
+        builder1.setCancelable(true);
+
+        builder1.setPositiveButton(
+                "Proceed",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Intent intent = new Intent(Config.this, Mylogin.class);
+                        startActivity(intent);
+
+                        //dialog.cancel();
+                    }
+                });
+
+        builder1.setNegativeButton(
+                "Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+    }
+
+    //fetch URLS
+
+    private void getUrls(){
+        String URLstring = "https://ushaurinode.mhealthkenya.co.ke/config";
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URLstring, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                Log.d("", response.toString());
+
+                try {
+                    urlModelArrayList = new ArrayList<>();
+                    names = new ArrayList<>();
+
+                    urlModelArrayList.clear();
+                    names.clear();
+
+                    for (int i=0; i<response.length(); i++){
+
+                        JSONObject jsonObject =response.getJSONObject(i);
+
+                        int url_id = jsonObject.getInt("id");
+                        String url_stage =jsonObject.getString("stage");
+                        String main_urls =jsonObject.getString("url");
+
+                        url_Model = new urlModel(url_id, url_stage, main_urls);
+                        names.add(url_Model);
+                        urlModelArrayList.add(url_Model.getStage());
+
+                    }
+
+                    names.add(new urlModel(0, "", "--select baseURL--"));
+                    urlModelArrayList.add("--select baseURL--");
+
+                    ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(Config.this, simple_spinner_item, urlModelArrayList);
+                    spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+                    spinner1.setAdapter(spinnerArrayAdapter);
+
+                    spinner1.setSelection(spinnerArrayAdapter.getCount()-1);
+                    dataId =names.get(spinnerArrayAdapter.getCount()-1).getId();
+
+                    spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                            dataId = names.get(position).getId();
+
+                            if (dataId==1){
+
+                                BASE_URL = names.get(position).getUrl();
+
+                            }
+                            else if(dataId==2){
+
+                                BASE_URL = names.get(position).getUrl();
+                            }
+
+
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(Config.this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
 
 
 }
---
+++ app/src/main/java/com/example/kenweezy/mytablayouts/Config/SelectUrl.java
@@ -0,0 +1,18 @@
+package com.example.kenweezy.mytablayouts.Config;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.kenweezy.mytablayouts.R;
+
+public class SelectUrl extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_url);
+
+
+    }
+}
